5f0700cab3e4f72ad58820f0e0f6879338a154e4
yugo <miyabe@fenrir.co.jp>
2022-10-07 23:14:37 +0900

    テキストのプレースフォルダーを追加

--- BasicLayoutsCodelab/app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
+++ BasicLayoutsCodelab/app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
@@ -24,9 +24,14 @@ import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
 import androidx.compose.material.TextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.codelab.basiclayouts.ui.theme.MySootheTheme
@@ -44,9 +49,17 @@ fun SearchBar(
     modifier: Modifier = Modifier
 ) {
     TextField(
-        value = "", onValueChange = {}, modifier = modifier
+        value = "",
+        onValueChange = {},
+        modifier = modifier
             .fillMaxWidth()
-            .heightIn(min = 56.dp)
+            .heightIn(min = 56.dp),
+        placeholder = {
+            Text(text = stringResource(id = R.string.placeholder_search))
+        },
+        leadingIcon = {
+            Icon(imageVector = Icons.Default.Search, contentDescription = null)
+        }
     )
 }
 
