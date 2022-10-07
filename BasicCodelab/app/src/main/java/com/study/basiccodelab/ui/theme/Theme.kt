e57bc22db681be869e29fc5a527c8e4d17c092bc
yugo <miyabe@fenrir.co.jp>
2022-10-07 21:40:13 +0900

    カラーの指定

--- BasicCodelab/app/src/main/java/com/study/basiccodelab/ui/theme/Theme.kt
+++ BasicCodelab/app/src/main/java/com/study/basiccodelab/ui/theme/Theme.kt
@@ -1,10 +1,9 @@
 package com.study.basiccodelab.ui.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.graphics.*
 
 private val DarkColorPalette = darkColors(
     primary = Purple200,
@@ -15,16 +14,13 @@ private val DarkColorPalette = darkColors(
 private val LightColorPalette = lightColors(
     primary = Purple500,
     primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
+    secondary = Teal200,
     background = Color.White,
     surface = Color.White,
     onPrimary = Color.White,
     onSecondary = Color.Black,
     onBackground = Color.Black,
     onSurface = Color.Black,
-    */
 )
 
 @Composable
