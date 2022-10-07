210e2207eedb3edb3b8ce109b307092217edaa1a
yugo <miyabe@fenrir.co.jp>
2022-10-07 21:49:21 +0900

    色の指定変更

--- BasicCodelab/app/src/main/java/com/study/basiccodelab/ui/theme/Theme.kt
+++ BasicCodelab/app/src/main/java/com/study/basiccodelab/ui/theme/Theme.kt
@@ -1,9 +1,11 @@
 package com.study.basiccodelab.ui.theme
 
-import androidx.compose.foundation.*
-import androidx.compose.material.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.graphics.*
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
     primary = Purple200,
@@ -12,15 +14,10 @@ private val DarkColorPalette = darkColors(
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200,
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
+    surface = Blue,
+    onSurface = Color.White,
+    primary = LightBlue,
+    onPrimary = Navy
 )
 
 @Composable
