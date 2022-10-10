9693dbb4ff1dff43c22e5b47e3841124fb6053a7
yugo-miyabe <yuni42yuyuyu@gmail.com>
2022-10-10 23:07:39 +0900

    委譲プロパティを使用

--- BasicStateCodelab/app/src/main/java/com/study/basicstatecodelab/WaterCounter.kt
+++ BasicStateCodelab/app/src/main/java/com/study/basicstatecodelab/WaterCounter.kt
@@ -5,20 +5,21 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun WaterCounter(modifier: Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        val count: MutableState<Int> = remember { mutableStateOf(0) }
+        var count by remember { mutableStateOf(0) }
         Text(
-            text = "You've had ${count.value} glasses.", modifier = modifier.padding(16.dp)
+            text = "You've had $count glasses.", modifier = modifier.padding(16.dp)
         )
-        Button(onClick = { count.value++ }, modifier = Modifier.padding(top = 8.dp)) {
+        Button(onClick = { count++ }, modifier = Modifier.padding(top = 8.dp)) {
             Text(text = "Add one")
         }
     }
