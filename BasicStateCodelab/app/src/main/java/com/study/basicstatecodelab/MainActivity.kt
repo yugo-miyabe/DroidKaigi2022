17ed6916b19f8294ee26c92260dce2c645673abe
yugo-miyabe <yuni42yuyuyu@gmail.com>
2022-10-10 22:40:28 +0900

    画面を作成

--- BasicStateCodelab/app/src/main/java/com/study/basicstatecodelab/MainActivity.kt
+++ BasicStateCodelab/app/src/main/java/com/study/basicstatecodelab/MainActivity.kt
@@ -4,14 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.study.basicstatecodelab.ui.theme.BasicStateCodelabTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,28 +22,12 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    WellnessScreen()
                 }
             }
         }
     }
 }
-
-
-@Composable
-fun WaterCounter(modifier: Modifier) {
-    val count = 0
-    Text(
-        text = "You've had $count glasses.",
-        modifier = modifier.padding(16.dp)
-    )
-}
-
-@Composable
-fun WellnessScreen(modifier: Modifier = Modifier) {
-    WaterCounter(modifier = modifier)
-}
-
 
 @Composable
 fun Greeting(name: String) {
