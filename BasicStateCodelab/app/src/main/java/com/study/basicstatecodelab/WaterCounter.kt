5ba15613ff5e12682d87659d17cee6a980f638d8
yugo-miyabe <yuni42yuyuyu@gmail.com>
2022-10-10 23:41:02 +0900

    rememberSaveableに変更

--- BasicStateCodelab/app/src/main/java/com/study/basicstatecodelab/WaterCounter.kt
+++ BasicStateCodelab/app/src/main/java/com/study/basicstatecodelab/WaterCounter.kt
@@ -9,6 +9,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -16,7 +17,7 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun WaterCounter(modifier: Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count by remember { mutableStateOf(0) }
+        var count by rememberSaveable { mutableStateOf(0) }
         if (count > 0) {
             var showTask by remember { mutableStateOf(true) }
             if (showTask) {
