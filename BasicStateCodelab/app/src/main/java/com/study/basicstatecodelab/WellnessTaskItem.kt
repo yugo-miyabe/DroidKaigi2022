5c4736ad9d399ca55195b5e0112828fb84e35e2d
yugo-miyabe <yuni42yuyuyu@gmail.com>
2022-10-24 19:00:26 +0900

    リストを削除する処理を追加

--- BasicStateCodelab/app/src/main/java/com/study/basicstatecodelab/WellnessTaskItem.kt
+++ BasicStateCodelab/app/src/main/java/com/study/basicstatecodelab/WellnessTaskItem.kt
@@ -11,7 +11,7 @@ import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -40,14 +40,14 @@ fun WellnessTaskItem(
 }
 
 @Composable
-fun WellnessTaskItem(taskName: String, modifier: Modifier = Modifier) {
-    var checkedState by remember { mutableStateOf(false) }
+fun WellnessTaskItem(taskName: String, onClose: () -> Unit, modifier: Modifier = Modifier) {
+    var checkedState by rememberSaveable { mutableStateOf(false) }
 
     WellnessTaskItem(
         taskName = taskName,
         checked = checkedState,
         onCheckedChanged = { newValue -> checkedState = newValue },
-        onClose = { },
+        onClose = onClose,
         modifier = modifier,
     )
 
