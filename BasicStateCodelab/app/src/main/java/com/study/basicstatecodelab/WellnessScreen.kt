724709f98ce6d8fa506506d8f15a1576a588ea6c
yugo-miyabe <yuni42yuyuyu@gmail.com>
2022-10-24 16:38:11 +0900

    削除

--- BasicStateCodelab/app/src/main/java/com/study/basicstatecodelab/WellnessScreen.kt
+++
@@ -1,9 +0,0 @@
-package com.study.basicstatecodelab
-
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-
-@Composable
-fun WellnessScreen(modifier: Modifier = Modifier) {
-    StatefulCounter(modifier = modifier)
-}
