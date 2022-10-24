334472754a63cc722df7d8ede3529e1c9149b409
yugo-miyabe <yuni42yuyuyu@gmail.com>
2022-10-24 17:43:59 +0900

    カウンタが10以上だと表示

--- BasicStateCodelab/app/src/main/java/com/study/basicstatecodelab/WaterCounter.kt
+++ BasicStateCodelab/app/src/main/java/com/study/basicstatecodelab/WaterCounter.kt
@@ -7,21 +7,23 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count by remember { mutableStateOf(0) }
+        var count by rememberSaveable { mutableStateOf(0) }
 
-        Text(
-            text = "You`ve had $count glasses.",
-            modifier = modifier.padding(16.dp)
-        )
-        Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
-            Text(text = "Add one")
+        if (count > 0) {
+            // This text is present if the button has been clicked
+            // at least once; absent otherwise
+            Text("You've had $count glasses.")
+        }
+        Button(onClick = { count++ }, Modifier.padding(top = 8.dp), enabled = count < 10) {
+            Text("Add one")
         }
     }
 }
