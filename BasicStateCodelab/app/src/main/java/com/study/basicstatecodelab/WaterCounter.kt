ab49ab9f35ebde4cb7b7b3ec81df5d1c449ded4e
yugo-miyabe <yuni42yuyuyu@gmail.com>
2022-10-24 17:23:56 +0900

    カウントアップする処理を追加

--- BasicStateCodelab/app/src/main/java/com/study/basicstatecodelab/WaterCounter.kt
+++ BasicStateCodelab/app/src/main/java/com/study/basicstatecodelab/WaterCounter.kt
@@ -1,16 +1,27 @@
 package com.study.basicstatecodelab
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-    val count = 0
-    Text(
-        text = "You`ve had $count glasses.",
-        modifier = modifier.padding(16.dp)
-    )
+    Column(modifier = modifier.padding(16.dp)) {
+        var count by remember { mutableStateOf(0) }
+
+        Text(
+            text = "You`ve had $count glasses.",
+            modifier = modifier.padding(16.dp)
+        )
+        Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
+            Text(text = "Add one")
+        }
+    }
 }
