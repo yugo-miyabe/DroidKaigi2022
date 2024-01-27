19896e540128bd52ce3aaa622a34168f7969c011
yugo-miyabe <yuni42yuyuyu@gmail.com>
2024-01-27 14:46:37 +0900

    コンポジションに保存する

--- BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/MainActivity.kt
+++ BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -40,13 +50,50 @@ fun WellnessScreen(modifier: Modifier = Modifier) {
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count = 0
-        Text(text = "You`ve had $count glasses.")
-        Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
-            Text(text = "Add one")
+        var count by remember { mutableStateOf(0) }
+        if (count > 0) {
+            var showTask by remember { mutableStateOf(true) }
+            if (showTask) {
+                WellnessTaskItem(
+                    onClose = { showTask = false },
+                    taskName = "Have you taken yor 15 minute walk today?",
+                )
+            }
+            Text(text = "You`ve had $count glasses.")
+        }
+        Row(Modifier.padding(top = 8.dp)) {
+            Button(
+                onClick = { count++ },
+                enabled = count < 10
+            ) {
+                Text(text = "Add one")
+            }
+            Button(
+                onClick = { count = 0 },
+                modifier = Modifier.padding(start = 8.dp),
+            ) {
+                Text(text = "Clear water count")
+            }
         }
     }
 }
+
+@Composable
+fun WellnessTaskItem(
+    taskName: String, onClose: () -> Unit, modifier: Modifier = Modifier
+) {
+    Row(modifier = modifier, verticalAlignment = Alignment.CenterVertically) {
+        Text(
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = 16.dp), text = taskName
+        )
+        IconButton(onClick = onClose) {
+            Icon(imageVector = Icons.Filled.Close, contentDescription = "Close")
+        }
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
