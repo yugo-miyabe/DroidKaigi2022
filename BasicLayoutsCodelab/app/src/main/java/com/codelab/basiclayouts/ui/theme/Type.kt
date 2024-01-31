fa5a920a21695d0960f37c3e475ffda39fb99b10
yugo-miyabe <yuni42yuyuyu@gmail.com>
2024-01-31 23:45:47 +0900

    タイポグラフィを設定

--- BasicLayoutsCodelab/app/src/main/java/com/codelab/basiclayouts/ui/theme/Type.kt
+++ BasicLayoutsCodelab/app/src/main/java/com/codelab/basiclayouts/ui/theme/Type.kt
@@ -21,7 +21,6 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
 import com.codelab.basiclayouts.R
 
@@ -50,60 +49,34 @@ private val fontFamilyLato = FontFamily(
 )
 
 val typography = Typography(
-    displayLarge = TextStyle(
-        fontFamily = fontFamilyKulim,
-        fontWeight = FontWeight.Light,
-        fontSize = 57.sp,
-        lineHeight = 64.sp,
-        letterSpacing = (-0.25).sp
+    headlineSmall = TextStyle(
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 24.sp,
+        lineHeight = 32.sp,
+        letterSpacing = 0.sp
     ),
-    displayMedium = TextStyle(
-        fontFamily = fontFamilyKulim,
-        fontSize = 45.sp,
-        lineHeight = 52.sp
-    ),
-    displaySmall = TextStyle(
-        fontFamily = fontFamilyKulim,
-        fontSize = 36.sp,
-        lineHeight = 44.sp
-    ),
-    titleMedium = TextStyle(
-        fontFamily = fontFamilyLato,
-        fontWeight = FontWeight(500),
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = (0.15).sp
-    ),
-    bodySmall = TextStyle(
-        fontFamily = fontFamilyLato,
-        fontSize = 12.sp,
-        lineHeight = 16.sp,
-        letterSpacing = (0.4).sp
-    ),
-    bodyMedium = TextStyle(
-        fontFamily = fontFamilyLato,
-        fontSize = 14.sp,
-        lineHeight = 20.sp,
-        letterSpacing = (0.25).sp
+    titleLarge = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 18.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp
     ),
     bodyLarge = TextStyle(
-        fontFamily = fontFamilyLato,
+        fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
-        letterSpacing = (0.5).sp
+        letterSpacing = 0.15.sp
     ),
-    labelMedium = TextStyle(
-        fontFamily = fontFamilyLato,
-        fontWeight = FontWeight(500),
-        fontSize = 12.sp,
-        lineHeight = 16.sp,
-        letterSpacing = (0.5).sp,
-        textAlign = TextAlign.Center
-    ),
-    labelLarge = TextStyle(
-        fontFamily = fontFamilyLato,
+    bodyMedium = TextStyle(
+        fontWeight = FontWeight.Medium,
         fontSize = 14.sp,
         lineHeight = 20.sp,
-        letterSpacing = (0.1).sp
+        letterSpacing = 0.25.sp
+    ),
+    labelMedium = TextStyle(
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp
     )
 )
