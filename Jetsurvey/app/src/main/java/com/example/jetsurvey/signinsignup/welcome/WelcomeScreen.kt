1387aa8d61cd8b1fc4992a975d4d8fa13dfab3de
yugo-miyabe <yuni42yuyuyu@gmail.com>
2024-02-12 23:33:53 +0900

    ライブラリアップデート

--- Jetsurvey/app/src/main/java/com/example/jetsurvey/signinsignup/welcome/WelcomeScreen.kt
+++ Jetsurvey/app/src/main/java/com/example/jetsurvey/signinsignup/welcome/WelcomeScreen.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
