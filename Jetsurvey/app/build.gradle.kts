0a55198ae91bed0112065e8e3ef6f4835b69a847
yugo-miyabe <yuni42yuyuyu@gmail.com>
2024-02-13 19:15:40 +0900

    ライブラリ追加

--- Jetsurvey/app/build.gradle.kts
+++ Jetsurvey/app/build.gradle.kts
@@ -59,6 +59,9 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("com.google.android.material:material")
+    // アイコン
+    implementation("androidx.compose.material:material-icons-extended")
     testImplementation("junit:junit:4.13.2")
     implementation("androidx.navigation:navigation-compose:2.5.3")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
