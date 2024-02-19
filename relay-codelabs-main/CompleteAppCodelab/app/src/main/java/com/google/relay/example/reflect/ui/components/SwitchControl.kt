73b0e7b84e85692014f1dfccb87634be96f5382e
yugo-miyabe <yuni42yuyuyu@gmail.com>
2024-02-19 16:38:59 +0900

    状態を追加

--- relay-codelabs-main/CompleteAppCodelab/app/src/main/java/com/google/relay/example/reflect/ui/components/SwitchControl.kt
+++ relay-codelabs-main/CompleteAppCodelab/app/src/main/java/com/google/relay/example/reflect/ui/components/SwitchControl.kt
@@ -40,9 +40,21 @@ import com.google.relay.example.reflect.switch.Switch
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun SwitchControl(
-    // TODO: Fill in this signature
+    trackerData: TrackerData,
+    modifier: Modifier = Modifier,
+    onLongClick: (() -> Unit)? = null
 ) {
-    // TODO: Fill in this body
+    Switch(
+        modifier
+            .height(64.dp)
+            .clip(shape = RoundedCornerShape(size = 32.dp))
+            .combinedClickable(onLongClick = onLongClick) {
+                trackerData.toggle()
+            },
+        emoji = trackerData.tracker.emoji,
+        title = trackerData.tracker.name,
+        isChecked = trackerData.isToggled()
+    )
 }
 
 @Preview
@@ -55,7 +67,5 @@ fun SwitchControllerPreview() {
             type = TrackerType.BOOLEAN
         )
     )
-    SwitchControl(
-        // TODO: Pass data to SwitchControl
-    )
+    SwitchControl(data)
 }
--- relay-codelabs-main/CompleteAppCodelab/app/src/main/java/com/google/relay/example/reflect/ui/components/TrackerControl.kt
+++ relay-codelabs-main/CompleteAppCodelab/app/src/main/java/com/google/relay/example/reflect/ui/components/TrackerControl.kt
@@ -17,8 +17,18 @@
 package com.google.relay.example.reflect.ui.components
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import com.google.relay.example.reflect.model.Tracker
 import com.google.relay.example.reflect.model.TrackerData
@@ -44,7 +54,26 @@ fun TrackerControl(
     Box(modifier = modifier) {
 
         // TODO: replace with Relay tracker components
-        Text(text = trackerData.tracker.toString())
+        when (trackerData.tracker.type) {
+            TrackerType.BOOLEAN ->
+                SwitchControl(
+                    trackerData = trackerData,
+                    onLongClick = { expanded = true }
+                )
+
+            TrackerType.RANGE -> {
+                RangeControl(
+                    trackerData = trackerData,
+                    onLongClick = { expanded = true }
+                )
+            }
+
+            TrackerType.COUNT ->
+                ValueControl(
+                    trackerData = trackerData,
+                    onLongClick = { expanded = true },
+                )
+        }
 
         DropdownMenu(
             expanded = expanded,
